d62d5e6c20766e363fbc7f6411437cc683955bf9
Odofin Oyejide <odofin@swipe.ng>
2023-07-16 16:13:52 +0100

    Created multi-module project

---
+++ src/main/java/mcb/com/signvet/SignvetApplication.java
@@ -0,0 +1,13 @@
+package mcb.com.signvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignvetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignvetApplication.class, args);
+    }
+
+}
