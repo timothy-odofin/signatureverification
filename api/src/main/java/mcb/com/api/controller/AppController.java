f0b26142ada4c60be7979e2d8f8a4595e75b2ced
Odofin Oyejide <odofin@swipe.ng>
2023-07-18 14:18:29 +0100

    Rename AccountController to AppController and updated Constant

--- api/src/main/java/mcb/com/api/controller/AppController.java
+++ api/src/main/java/mcb/com/api/controller/AppController.java
@@ -1,10 +1,15 @@
 package mcb.com.api.controller;
 
 import static mcb.com.api.utils.ApiPath.*;
+
+import lombok.RequiredArgsConstructor;
+import mcb.com.api.service.AppService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(ACCOUNT_PATH)
-public class AccountController {
+@RequiredArgsConstructor
+public class AppController {
+    private final AppService appService;
 }
--- api/src/main/java/mcb/com/api/utils/Constant.java
+++ api/src/main/java/mcb/com/api/utils/Constant.java
@@ -2,4 +2,8 @@ package mcb.com.api.utils;
 
 public class Constant {
     public static String HOD="HOD";
+    public static String PAGE="page";
+    public static String SIZE="size";
+    public static String DEFAULT_PAGE="0";
+    public static String DEFAULT_SIZE="50";
 }
